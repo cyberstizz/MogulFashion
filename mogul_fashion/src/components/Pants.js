d02e062d9f5c5017f8a8a68c0e613876ccf8f404
Charles <charles.lamb.dev@gmail.com>
2023-09-27 12:04:49 -0400

    added the beginning of the pants componnet

---
+++ mogul_fashion/src/components/Pants.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Pants.scss';
+import SexHeader from './SexHeader';
+
+
+const Pants = () => {
+
+
+    return(
+        <React.Fragment>
+            <header>
+
+            </header>
+
+        </React.Fragment>
+    )
+}
