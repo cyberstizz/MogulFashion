a3db04ef7676437f1e82c275da5710c10ebd0272
Charles <charles.lamb.dev@gmail.com>
2023-09-27 12:18:27 -0400

    added the routes for the entire site

--- mogul_fashion/src/App.js
+++ mogul_fashion/src/App.js
@@ -9,28 +9,14 @@ function App() {
           
     <Routes>
     <Route path='/' element={<Home />} />
-    <Route path='/newArrivals' element={<NewArrivals />} />
-    <Route path='/bestSellers' element={<BestSellers />} />
-    <Route path='/discounts' element={<Discounts />} />
-    <Route path='/search/:query' element={<Search />} />
-    <Route path='/products/:productId' element={<ProductDescription />} />
-    <Route path='/createPage' element={<CreatePage />} />
-    <Route path='/deletePage' element={<DeletePage />} />
-    <Route path='products/:user/Cart/:user' element={<Cart />} />
-    <Route path='/test' element={<TestPage />} />
-    <Route path='/Cart/:user' element={<Cart />} />
-    <Route path='/success' element={<Success />} />
-    <Route path='/settings' element={<Settings />} />
-    <Route path='/about_us' element={<AboutUs />} />
-    <Route path='/contact' element={<Contact />} />
-    <Route path='/privacy_policy' element={<PrivacyPolicy />} />
+    <Route path='/sneakers' element={<Sneakers />} />
+    <Route path='/pants' element={<Pants />} />
+    <Route path='/hoodies' element={<Hoodies />} />
+    <Route path='/dresses' element={<Dresses />} />
+    <Route path='/skirts' element={<Skirts />} />
+    <Route path='/sets' element={<Sets />} />
     <Route path='/cookie_policy' element={<CookiePolicy />} />
     <Route path='/terms_of_use' element={<TermsOfUse />} />
-
-
-
-
-
     </Routes>
     <Footer />
     </Router>
---
+++ mogul_fashion/src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return(
+        <footer>this will be the footer</footer>
+    )
+}
