09683587ad208a5727860692a26f17e30d34d706
Charles <charles.lamb.dev@gmail.com>
2023-09-28 22:43:27 -0400

    added the subMenuComponent and its corresponding scss file

---
+++ mogul_fashion/src/components/subMenuComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './subMenuComponentt.scss';
+
+
+const subMenuComponent = (props) => {
+    return (
+        <React.Fragment>
+
+            
+        </React.Fragment>
+    )
+}
