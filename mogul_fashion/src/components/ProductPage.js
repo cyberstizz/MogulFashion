61bf46b51959368cd68798205cda80ab7803292f
Charles <charles.lamb.dev@gmail.com>
2023-10-01 16:22:49 -0400

    changes to the product page. I added the search icon the search  bar and the arrow to go bak

--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -14,8 +14,9 @@ const ProductPage = () => {
                 <Hamburger />
                 <span className='pantsHeader'><SexHeader header="Pants" marginLeft="4vw" /></span>
             </header>
-            <section className="searchBar"><i class="fa fa-search" aria-hidden="true" style={{fontSize: "30px", marginLeft: "2vw", marginTop: "1vh"}}></i> Search</section>
-
+            <section className="searchBar"><i class="fa fa-search" aria-hidden="true" style={{fontSize: "30px", marginLeft: "2vw", marginTop: "1vh"}}></i> <span style={{marginLeft: "3vw", fontSize: "35px"}}>Search</span></section>
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+            <section className="underSearchBar"></section>
             <Link to="/"><div>these is the product page, and this page links back home</div></Link>
         </React.Fragment>
     )
