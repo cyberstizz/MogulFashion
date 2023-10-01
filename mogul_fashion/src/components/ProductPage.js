11c2b793dfc5c071afe88d61dfe96017fba5a35a
Charles <charles.lamb.dev@gmail.com>
2023-10-01 10:00:55 -0400

    changes to the product page

--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 
 const ProductPage = () => {
     return(
+        <React.Fragment>
+        <header className='ProductTop'>
+        <Hamburger />
+        <span className='pantsHeader'><SexHeader header="Pants" marginLeft="4vw" /></span>
+    </header>
         <Link to="/"><div>these is the product page, and this page links back home</div></Link>
+        </React.Fragment>
     )
 }
 
