91bdc74469fb994fcee1ba974c454cfc0f777264
Charles <charles.lamb.dev@gmail.com>
2023-10-01 10:40:45 -0400

    added the search bar without the search icon to the top of the product page

--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -1,15 +1,20 @@
 import React from "react";
+import './ProductPage.scss';
 import './Skirts.scss';
 import { Link } from 'react-router-dom';
+import Hamburger from './Hamburger';
+import SexHeader from './SexHeader'
 
 const ProductPage = () => {
     return(
         <React.Fragment>
-        <header className='ProductTop'>
-        <Hamburger />
-        <span className='pantsHeader'><SexHeader header="Pants" marginLeft="4vw" /></span>
-    </header>
-        <Link to="/"><div>these is the product page, and this page links back home</div></Link>
+            <header className='ProductTop'>
+                <Hamburger />
+                <span className='pantsHeader'><SexHeader header="Pants" marginLeft="4vw" /></span>
+            </header>
+            <section className="searchBar">Search</section>
+
+            <Link to="/"><div>these is the product page, and this page links back home</div></Link>
         </React.Fragment>
     )
 }
