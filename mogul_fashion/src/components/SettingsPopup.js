9b30621fcefe21ab56af2b5763e8208ded01dbdf
Charles <charles.lamb.dev@gmail.com>
2023-10-08 17:05:58 -0400

    added a link back home inot the settins popup

--- mogul_fashion/src/components/SettingsPopup.js
+++ mogul_fashion/src/components/SettingsPopup.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './SettingsPopup.scss'
+import { Link } from "react-router-dom";
+import Home from "./Home";
 
 
 const SettingsPopup = () => {
     return(
-        <main className="fullSettingsPopupBlock">
-
-            
+      <Link to={Home}> <main className="fullSettingsPopupBlock">
 
 
-        </main>
+
+
+        </main></Link> 
     )
 }
