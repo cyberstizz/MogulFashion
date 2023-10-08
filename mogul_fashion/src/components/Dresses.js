9ab8b71ee4f286c444d0a6cc31fe17056259c5c8
Charles <charles.lamb.dev@gmail.com>
2023-10-07 20:18:08 -0400

    added default links to the skirts dresses sets and hoodies pages

--- mogul_fashion/src/components/Dresses.js
+++ mogul_fashion/src/components/Dresses.js
@@ -5,7 +5,27 @@ import { Link } from 'react-router-dom';
 
 const Dresses = () => {
     return(
-        <Link to="/products"><div>these are the dresses and this links to the product page</div></Link>
+        <React.Fragment>
+
+            <header className='ProductTop'>
+                <Hamburger />
+                <span className='pantsHeader'><SexHeader header="Pants" marginLeft="4vw" /></span>
+            </header>
+
+            <nav className='subMenuFullBlock'>
+                <ul className='submenuHeader'>
+                    <li className='submenuNavItem'>Sneakers</li>
+                    <li className='submenuNavItem'>Pants</li>
+                    <li className='submenuNavItem'>Hoodies</li>
+                </ul>
+            </nav>
+            <main className='submenuBody'>
+            <Link to="/products"><SubMenuComponent name='blue jeans'/></Link>
+            <Link to="/products"><SubMenuComponent name='blue jeans' /></Link>
+            <Link to="/products"><SubMenuComponent name='blue jeans' /></Link>
+            </main>
+           
+        </React.Fragment>
     )
 }
 
