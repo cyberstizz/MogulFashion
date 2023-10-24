f2fd512d3af19f008300ba76fe68f441c0aedb7c
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-10-24 01:04:30 -0400

    added more liks to te settings popul module

--- mogul_fashion/src/components/SettingsPopup.js
+++ mogul_fashion/src/components/SettingsPopup.js
@@ -5,15 +5,23 @@ import { Link } from "react-router-dom";
 
 const SettingsPopup = () => {
     return(
-      <Link to='/'> <main className="fullSettingsPopupBlock">
+      <Link to='/'> <nav className="fullSettingsPopupBlock">
+        <header></header>
+        <Link to='/' style={{ textDecoration: 'none', color: 'inherit', overflow: 'hidden' }}><img src='./MogulLogo.png' height="auto" alt='the mogo logo' className='thisHomeLogo'></img></Link>
+        <ul classname>
+        <li className="comingSoon">Home</li>
+        <li className="comingSoon">Pants</li>
+        <li>sneakers</li>
+        <li>pants</li>
+        <li>hoodies</li>
+        <li>dresses</li>
+        <li>skirts</li>
+        <li>sets</li>
 
-        <span className="comingSoon">Coming soon!</span>
-        <br />
-        <span className="comingSoon"> click anywhere to go back</span>
+        </ul>
 
 
-
-        </main></Link> 
+        </nav></Link> 
     )
 }
 
