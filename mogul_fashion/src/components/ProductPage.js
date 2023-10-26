a9bd7794f84850a786c40e5168763c3fc6ef991b
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-10-25 20:54:39 -0400

    added the enite product card component

--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -7,10 +7,12 @@ import { Link } from 'react-router-dom';
 const ProductPage = () => {
     return(
         <React.Fragment>
-            <section className="searchBar"><i class="fa fa-search" aria-hidden="true" style={{fontSize: "30px", marginLeft: "2vw", marginTop: "1vh"}}></i> <span style={{marginLeft: "3vw", fontSize: "35px"}}>Search</span></section>
             <i class="fa fa-arrow-left" aria-hidden="true"></i>
             <section className="underSearchBar"></section>
-            <Link to="/"><div>this is the product page, and this page links back home</div></Link>
+            <main className="entireProductCard">
+                
+                
+            </main>
         </React.Fragment>
     )
 }
