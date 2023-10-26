13b56da0bf7f5f23c85359b3420143fd40291afa
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-10-26 02:14:32 -0400

    added all products object that will serve as the datastore

---
+++ mogul_fashion/src/components/AllProducts.js
@@ -0,0 +1,99 @@
+import './App.scss';
+import React from 'react';
+
+const AllProducts = {
+    men : {
+        sneakers : {
+            spades : {
+                title: 'Spades',
+                headline: 'Combining athleticism with style',
+                imagePath: './spades.jpeg',
+                description: 'Introducing the "Spades Kick" — a contemporary sneaker artfully designed for the modern enthusiast. Its distinct spade patterns, intricately woven into the shoe\'s fabric, are a nod to classic card designs. The shoe\'s sleek curves, paired with its bold lace and cushioned sole, meld both comfort and style, making it a must-have for any footwear aficionado.'
+            }, 
+            southOxford : {
+                title: 'South Oxford',
+                headline: '',
+                imagePath: './SouthOxford.jpg',
+                description: ''
+            },
+            maz : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            }
+        }, 
+        pants : {
+            jamaaca : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            },
+            colorVienz : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            },
+            mogulPants : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            }
+        }, 
+        hoodies : {
+            xHoodie : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            }
+        }
+    },
+    woman : {
+        dresses : {
+            theHula : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            },
+            theMilitia : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            },
+            theTiff : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            }
+        },
+        skirts : {
+            theQueen : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            },
+            theRoyalty : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: ''
+            }
+        }, 
+        sets : {
+            theGarterSet : {
+                title: '',
+                headline: '',
+                imagePath: '',
+                description: '' 
+            }
+        }
+    }
+}
--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -4,7 +4,7 @@ import './Skirts.scss';
 import { Link } from 'react-router-dom';
 
 
-const ProductPage = () => {
+const ProductPage = (props) => {
     return(
         <React.Fragment>
             <i class="fa fa-arrow-left" aria-hidden="true"></i>
@@ -21,6 +21,8 @@ const ProductPage = () => {
             </div>
 
             <section className="headlineText">Product Details</section>
+
+            <section className="productDescription">Introducing the 'Spades Kick' — a contemporary sneaker artfully designed for the modern enthusiast. Its distinct spade patterns, intricately woven into the shoe's fabric, are a nod to classic card designs. The shoe's sleek curves, paired with its bold lace and cushioned sole, meld both comfort and style, making it a must-have for any footwear aficionado.</section>
 
             </main>
         </React.Fragment>
