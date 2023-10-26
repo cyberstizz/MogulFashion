54d4ca39f21559c446ea3aeea6ce1eaf57b65b4c
Charles Lamb Jr <charleslambjr@Charless-MacBook-Pro.local>
2023-10-25 22:26:32 -0400

    added the majority of the product card. still missing the description and some sizing and fonts

--- mogul_fashion/src/components/ProductPage.js
+++ mogul_fashion/src/components/ProductPage.js
@@ -10,8 +10,18 @@ const ProductPage = () => {
             <i class="fa fa-arrow-left" aria-hidden="true"></i>
             <section className="underSearchBar"></section>
             <main className="entireProductCard">
-                
-                
+            <div className="headerWrapper">
+                <header className="productFont">Spades</header>
+                <div className="headerFooter"></div>
+            </div>
+            <section className="headlineText">Combining athleticism with style</section>
+            <div className="headerWrapper">
+                <img className="productImage" src="./spades.jpeg" />
+                <div className="headerFooterImage"></div>
+            </div>
+
+            <section className="headlineText">Product Details</section>
+
             </main>
         </React.Fragment>
     )
